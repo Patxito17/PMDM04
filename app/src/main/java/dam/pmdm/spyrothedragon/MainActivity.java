c852e91956f64c56a75396dde97b36a1245be176
Patxito17 <estampador.avellana_0n@icloud.com>
2025-01-21 11:14:37 +0100

    guide step 0: introduction

--- app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
+++ app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
@@ -3,6 +3,10 @@ package dam.pmdm.spyrothedragon;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -26,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Configurar Toolbar
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
         if (navHostFragment != null) {
             navController = NavHostFragment.findNavController(navHostFragment);
@@ -41,20 +49,29 @@ public class MainActivity extends AppCompatActivity {
                     destination.getId() == R.id.navigation_collectibles) {
                 // Para las pantallas de los tabs, no queremos que aparezca la flecha de atrás
                 getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            }
-            else {
+            } else {
                 // Si se navega a una pantalla donde se desea mostrar la flecha de atrás, habilítala
                 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             }
         });
+
+        ImageView diamondImage = binding.getRoot().findViewById(R.id.diamondImage);
+        diamondImage.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale_pulse));
+
+        diamondImage.setOnClickListener(v -> {
+            v.clearAnimation();
+            binding.includeLayout.guide0Introduction.setVisibility(View.GONE);
+        });
+
+        ImageView logoSpyro = binding.getRoot().findViewById(R.id.logoSpyro);
+        logoSpyro.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale_rotate_in));
 
     }
 
     private boolean selectedBottomMenu(@NonNull MenuItem menuItem) {
         if (menuItem.getItemId() == R.id.nav_characters)
             navController.navigate(R.id.navigation_characters);
-        else
-        if (menuItem.getItemId() == R.id.nav_worlds)
+        else if (menuItem.getItemId() == R.id.nav_worlds)
             navController.navigate(R.id.navigation_worlds);
         else
             navController.navigate(R.id.navigation_collectibles);
@@ -87,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton(R.string.accept, null)
                 .show();
     }
-
 
 
 }
